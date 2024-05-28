8e770a1ef6f854a0fb7d3a6590eb917cab3622a4
Kaique Savi <kaiquesavi1@gmail.com>
2024-05-28 16:00:46 -0300

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"green": "#00E16F",
+				'dark-blue': '#000C5C',
+				'light-blue': '#00C2FF'
+			},
+			fontFamily: {
+				"Baibamjuree": 'BaiBamjuree',
+			},
+			backgroundImage: {
+				'Planos': "url('./public/images/bgPlanos.png')"
+			}
+		},
+	},
+	plugins: [],
+}
