46ff41a19f08953ae58f03588bc42c6dbf62972a
Kaique Savi <kaiquesavi1@gmail.com>
2024-05-30 10:07:00 -0300

    Teste

--- .wrangler/tmp/dev-0lJpZd/chunks/planos-empresariais_B4vAF3kD.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/planos-empresariais_CkONlan8.mjs');
-
-export { page };
--- .wrangler/tmp/pages-dXR1zH/chunks/fale-conosco_C-s4SRxm.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/fale-conosco_DKLju08u.mjs').then(n => n.f);
-
-export { page };
