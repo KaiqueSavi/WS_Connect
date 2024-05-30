08ebb836679cdd52866198d8032f858938b0bbee
Kaique Savi <kaiquesavi1@gmail.com>
2024-05-30 10:36:35 -0300

    teste

--- .wrangler/tmp/dev-FGf8lL/chunks/planos-residenciais_DzDdWmxb.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/planos-residenciais_m_p_8GoZ.mjs');
-
-export { page };
